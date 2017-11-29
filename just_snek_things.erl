aa5f05dff7f78009849d0f20892e2cf7723fd254
Anne Oursler <anneoursler@gmail.com>
2017-11-29 16:05:36 -0500

    Added handle_cast functions to deal with move directions from client

--- just_snek_things.erl
+++ just_snek_things.erl
@@ -17,32 +17,6 @@
 -export([init/1, terminate/2, handle_call/3, handle_cast/2]).
 -export([filterOut/2]).
 
-% STUB FUNCTION DEFINITIONS
-% handle_call({move, UserName, Direction}, _From, LoopData) ->
-%    % TODO: Use erlport to send username and direction to snek game.
-%    io:fwrite("~s would like to move ~s ~n", [UserName, Direction]),
-%    {reply, LoopData}.
-
-% move(HostName, GameName, UserName) ->
-%    %TODO: Get arow key press or --quit
-%    %      Without prompt & try to hide keypress as well?
-%    Move = io:get_line("~w: ", [UserName]),
-%    case Move of
-%        "--quit\n" -> {ok};
-%        Move ->
-%            gen_server:call({GameName, HostName},
-%                            {move, Move, {UserName, node()}}),
-%            move(HostName, GameName, UserName)
-%    end.
-
-% receiveMessages() ->
-%    receive
-%        {{Sender, _Node}, Board} -> io:fwrite("~w: ~s", [Sender, Board]) %TODO: Make board 2D array, print 2D array
-%        %TODO: Send board to erlport to display??
-%    end,
-%    receiveMessages().
-
-
 
 % CLIENT GLOBAL CONSTANTS
 boardWidth() -> 160.
@@ -61,8 +35,6 @@ join_game(HostName, GameName, UserName) ->
     unsubscribe(HostName, GameName, UserName).
 
 move(HostName, GameName, UserName) ->
-    %TODO ANNE: Get arow key press or --quit
-    %      Without prompt & try to hide keypress as well?
     Move = io:get_chars("", 1),
     io:fwrite("got a char~n"),
     case Move of
@@ -88,28 +60,44 @@ subscribe(HostName, GameName, UserName) ->
 unsubscribe(HostName, GameName, UserName) ->
     gen_server:cast({GameName, HostName}, {unsubscribe, {UserName, node()}}).
 
+
 % SERVER FUNCTIONS
-start_link(ChatName) ->
-    {ok, Pid} = gen_server:start_link({global, ChatName}, ?MODULE, [],[]),
+start_link(GameName) ->
+    {ok, Pname} = python:start(),
+    {ok, Pid} = gen_server:start_link({global, GameName}, ?MODULE, [Pname],[]),
     io:fwrite("server_started~n"),
-    register(ChatName, Pid),
-    {ChatName, Pid}.
-stop(ChatName) ->
-    unregister(ChatName),
-    gen_server:stop({global, ChatName}),
+    register(GameName, Pid),
+    {GameName, Pid}.
+stop(GameName) ->
+    unregister(GameName),
+    gen_server:stop({global, GameName}),
     io:fwrite("server_stopped~n").
-init(LoopData) ->
-    {ok, LoopData}.
-handle_cast({subscribe, UserName}, LoopData) ->
+init(Pname) ->
+    {ok, {Pname, []}}.
+handle_cast({subscribe, UserName}, {Pname, LoopData}) ->
     io:fwrite("subscribing~n"),
-    {noreply, [UserName | LoopData]};
-handle_cast({unsubscribe, UserName}, LoopData) ->
+    {noreply, {Pname, [UserName | LoopData]}};
+handle_cast({unsubscribe, UserName}, {Pname, LoopData}) ->
     io:fwrite("unsubscribing~n"),
-    {noreply, filterOut(UserName, LoopData)}.
-handle_call({move, UserName, Direction}, _From, LoopData) ->
-    % TODO MATT: Use erlport to send username and direction to snek game.
-    io:fwrite("~s would like to move ~s ~n", [UserName, Direction]),
-    {reply, LoopData}.
+    {noreply, {Pname, filterOut(UserName, LoopData)}};
+handle_cast({"move_left", UserName}, {Pname, LoopData}) ->
+    io:fwrite("moving left ~n"),
+    python:call(Pname, snek, move, [UserName, "a"]),  
+    {noreply, {Pname, LoopData}};
+handle_cast({"move_right", UserName}, {Pname, LoopData}) ->
+    io:fwrite("moving right ~n"),
+    python:call(Pname, snek, move, [UserName, "d"]),  
+    {noreply, {Pname, LoopData}};
+handle_cast({"move_up", UserName}, {Pname, LoopData}) ->
+    io:fwrite("moving up ~n"),
+    python:call(Pname, snek, move, [UserName, "w"]),  
+    {noreply, {Pname, LoopData}};
+handle_cast({"move_down", UserName}, {Pname, LoopData}) ->
+    io:fwrite("moving down ~n"),
+    python:call(Pname, snek, move, [UserName, "s"]),
+    {noreply, {Pname, LoopData}}.
+handle_call(_Request, _From, State) ->
+    {reply, State}.
 
 terminate(Reason, _LoopData) ->
     io:fwrite("server_stopping~n"),
